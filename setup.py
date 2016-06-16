90201eff85b1c4801519e4e9575e55df38c5c186
Anthony Shaw <anthonyshaw@apache.org>
2016-06-16 16:09:41 +1000

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='sciencelogic',
-    version='0.1.0',
+    version='0.2.0',
     description="Client library for sciencelogic EM7",
     long_description=readme + '\n\n' + history,
     author="Anthony Shaw",
