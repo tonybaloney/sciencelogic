de6f0be2ae1643cfdccaab4e7095bfd47f1691b9
S. John Banner <jbanner@cisco.com>
2018-05-29 10:56:17 +0200

    Adjust code to get the counters we need.

--- mtv5-links.py
+++ mtv5-links.py
@@ -15,12 +15,17 @@ def collect_info(userid, password, hostname, dev_info):
     :param dev_info:
     :return:
     """
+    dev_data_list = []
     c = Client(userid, password, dev_info["url"])
 
     # Get the first device
     devices = c.devices(details=True, options=["filter.name.contains={}".format(hostname)])
     print("Device details for {}:".format(hostname))
     for d in devices:
+        dev_data = {
+            'hostname': hostname,
+            'ints': []
+        }
         # print("device details:", d.details)
         print(">", d.details['ip'])
         # pprint.pprint(d.details)
@@ -28,22 +33,27 @@ def collect_info(userid, password, hostname, dev_info):
         int_resp = c.get(d.details['interfaces']['URI'])
         int_list = json.loads(int_resp.text)
         for int_ref in int_list['result_set']:
+            int_perf_data = {}
             if int_ref['description'] in dev_info["ints"]:
                 print(">>")
                 pprint.pprint(int_ref)
 
                 int_detail = c.get(int_ref['URI'])
                 interface = json.loads(int_detail.text)
-                print(">>>", interface['alias'])
+                print(">>>", interface['alias'], "/", interface['measure'])
+                int_perf_data['int'] = int_ref['description']
                 # pprint.pprint(interface)
 
-                # # Get a list of available performance counters
-                # counters = interface.performance_counters()
-                #
-                # print("Available counters")
-                # for counter in counters:
-                #     print("{}".format(counter.name()))
-    return()
+                if 'normalized_hourly' in interface['interface_data']:
+                    int_perf_data['data'] = json.loads(
+                        c.get(interface['interface_data']['normalized_hourly']['URI']).text)
+                elif 'data' in interface['interface_data']:
+                    int_perf_data['data'] = json.loads(
+                        c.get(interface['interface_data']['data']['URI']).text)
+            if int_perf_data:
+                dev_data['ints'].append(int_perf_data)
+
+    return(dev_data)
 
     # Get historic performance data of the first counter
     data = counters[0].get_presentations()[0].get_data()
